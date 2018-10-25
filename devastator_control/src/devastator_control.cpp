e5807958375cf72c746cc26d5d7289a599566ca1
pandoras-actor13 <silvercrowalpha@gmail.com>
2018-10-25 15:41:20 +0800

    added documentation. Robot control and override now in main loop.

--- devastator_control/src/devastator_control.cpp
+++ devastator_control/src/devastator_control.cpp
@@ -1,3 +1,8 @@
+//  Devastator bot control module
+//  October 23, 2018
+//  DOST-ASTI, RDD Team
+/////////////////////////////////////////////////////////////
+
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/Joy.h>
@@ -6,23 +11,32 @@
 #include <std_msgs/Bool.h>
 #include <geometry_msgs/Twist.h>
 
-
-
 class Devastator {
 public:
   Devastator();
-  //void BuzzPublisher();
-
-private:
-  void VelCallback(const geometry_msgs::Twist::ConstPtr& twist);
-  void OverrideCallback(const std_msgs::Bool::ConstPtr& override_msg);
-  //void DetectCallback(const std_msgs::Int32::ConstPtr& detect_msg);
-  void AutoNav_Control();
-  void ManualNav_Control();
-  void AssessMove();
+  //Create a public class member to publish flags used for robot movement.
   void FlagPublisher(int x);
 
+  //Set Get sensor data variables.
+  float sonic_FF_data();
+  float sonic_FD_data();
+  float aIR_FR_data();
+  float aIR_FL_data();
+  float dIR_F_data();
+  float dIR_BR_data();
+  float dIR_BL_data();
 
+  //Override and geometry variables.
+  bool override_flag();
+  float* vel_data();
+
+
+private:
+  //Override Flags and geometry data subscribers.
+  void VelCallback(const geometry_msgs::Twist::ConstPtr& twist);
+  void OverrideCallback(const std_msgs::Bool::ConstPtr& override_msg);
+
+  //Sensor data subscribers
   void sonic_FF_Callback(const sensor_msgs::Range::ConstPtr& sonic_FF_msg);
   void sonic_FD_Callback(const sensor_msgs::Range::ConstPtr& sonic_FD_msg);
   void aIR_FR_Callback(const sensor_msgs::Range::ConstPtr& aIR_FR_msg);
@@ -31,6 +45,7 @@ private:
   void dIR_BR_Callback(const sensor_msgs::Range::ConstPtr& dIR_BR_msg);
   void dIR_BL_Callback(const sensor_msgs::Range::ConstPtr& dIR_BL_msg);
 
+  //ROS variables
   ros::NodeHandle nh_;
   ros::Publisher nav_flag_pub;
   ros::Publisher buzz_pub;
@@ -54,12 +69,6 @@ private:
   float dIR_BR_range;
   float dIR_BL_range;
   float linear_,angular_;
-  bool override;
-  bool first_buzz;
-  int detect;
-
-  std_msgs::Int32 buzz_flag;
-
 };
 
 
@@ -82,20 +91,16 @@ Devastator::Devastator()
 void Devastator::OverrideCallback(const std_msgs::Bool::ConstPtr& override_msg)
 {
   override = override_msg->data;
-  if (override){
-    ManualNav_Control();
-  }
-  else {
-    AutoNav_Control();
-  }
 }
 
+//Get geometry data for robot displacement values from ROS geometry publisher.
 void Devastator::VelCallback(const geometry_msgs::Twist::ConstPtr& vel_msg)
 {
   linear_ = vel_msg->linear.x;
   angular_ = vel_msg->angular.z;
 }
 
+//Get sensor values from individual sensor publishers.
 void Devastator::sonic_FF_Callback(const sensor_msgs::Range::ConstPtr& sonic_FF_msg)
 {
   sonic_FF_range = sonic_FF_msg->range;
@@ -128,115 +133,50 @@ void Devastator::dIR_BL_Callback(const sensor_msgs::Range::ConstPtr& dIR_BL_msg)
   dIR_BL_range = dIR_BL_msg->range;
 }
 
-void Devastator::AutoNav_Control()
+
+//Get Functions
+bool Devastator::override_flag()
 {
-  float sonic_FF_zone = 0.30;
-  float sonic_FD_zone = 0.40;
-  float aIR_FR_zone = 0.30;
-  float aIR_FL_zone = 0.30;
-  float dIR_F_zone = 0.0;
-  float dIR_BR_zone = 1.0;
-  float dIR_BL_zone = 1.0;
-
-  if (aIR_FR_range > aIR_FR_zone && aIR_FL_range > aIR_FL_zone)
-  {
-    if (sonic_FD_range > sonic_FD_zone && sonic_FF_range > sonic_FF_zone and dIR_F_range==dIR_F_zone)
-      FlagPublisher(1);
-    else
-    {
-      if (dIR_BR_range && dIR_BL_range)
-      {
-        FlagPublisher(5);
-        if (aIR_FR_range > aIR_FL_range)
-          FlagPublisher(2);
-        else if (aIR_FL_range > aIR_FR_range)
-          FlagPublisher(3);
-        else
-          FlagPublisher(4);
-      }
-      else
-      {
-        if (aIR_FR_range > aIR_FL_range)
-          FlagPublisher(2);
-        else if (aIR_FL_range > aIR_FR_range)
-          FlagPublisher(3);
-        else
-          FlagPublisher(4);
-      }
-    }
-
-  }
-
-  else if (aIR_FR_range > aIR_FR_zone && aIR_FL_range <= aIR_FL_zone)
-    FlagPublisher(2);
-
-  else if (aIR_FR_range <= aIR_FR_zone && aIR_FL_range > aIR_FL_zone)
-    FlagPublisher(3);
-
-  else if (aIR_FR_range <= aIR_FR_zone && aIR_FL_range <= aIR_FL_zone)
-  {
-    if (dIR_BR_range && dIR_BL_range)
-      {
-        FlagPublisher(5);
-        if (aIR_FR_range > aIR_FL_range)
-          FlagPublisher(2);
-        else if (aIR_FL_range > aIR_FR_range)
-          FlagPublisher(3);
-        else
-          FlagPublisher(4);
-      }
-
-    else if (!dIR_BR_range && !dIR_BL_range)
-    {
-      if ((sonic_FD_range > sonic_FD_zone && sonic_FF_range > sonic_FF_zone && dIR_F_range==dIR_F_zone) ||
-      (sonic_FD_range > sonic_FD_zone && sonic_FF_range > sonic_FF_zone && dIR_F_range != dIR_F_zone) ||
-      (sonic_FD_range > sonic_FD_zone && sonic_FF_range <= sonic_FF_zone && dIR_F_range==dIR_F_zone) ||
-      (sonic_FD_range <= sonic_FD_zone && sonic_FF_range > sonic_FF_zone && dIR_F_range==dIR_F_zone))
-      {
-        if (aIR_FR_range > aIR_FL_range)
-          FlagPublisher(2);
-        else if (aIR_FL_range > aIR_FR_range)
-          FlagPublisher(3);
-        else
-          FlagPublisher(4);
-      }
-      else
-        FlagPublisher(6);
-    }
-
-    else if ((!dIR_BR_range && dIR_BL_range) || (dIR_BR_range && !dIR_BL_range))
-    {
-      if (aIR_FR_range > aIR_FL_range)
-          FlagPublisher(2);
-        else if (aIR_FL_range > aIR_FR_range)
-          FlagPublisher(3);
-        else
-          FlagPublisher(4);
-    }
-  }
-  else
-    FlagPublisher(5);
-
+  return override;
 }
 
-void Devastator::ManualNav_Control()
+float* Devastator::vel_data()
 {
-  //Reset to 0.0 any inconsistent readings.
-  if (linear_  == -0.0) linear_ = 0.0;
-  if (angular_ == -0.0) angular_ = 0.0;
-
-  if (linear_ < 0.0) {
-    FlagPublisher(5);
-  }else if (linear_ > 0.0) {
-    FlagPublisher(1);
-  }else if (angular_ < 0.0) {
-    FlagPublisher(2);
-  }else if (angular_ > 0.0) {
-    FlagPublisher(3);
-  }else
-    FlagPublisher(6);
+  float* vel = new float[2];
+  vel[0] = linear_;
+  vel[1] = angular_;
+  return vel;
+}
+float Devastator::sonic_FF_data()
+{
+  return sonic_FF_range;
 }
 
+float Devastator::sonic_FD_data()
+{
+  return sonic_FD_range;
+}
+
+float Devastator::aIR_FR_data()
+{
+  return aIR_FR_range;
+}
+float Devastator::aIR_FL_data()
+{
+  return aIR_FL_range;
+}
+float Devastator::dIR_F_data()
+{
+  return dIR_F_range;
+}
+float Devastator::dIR_BR_data()
+{
+  return dIR_BR_range;
+}
+float Devastator::dIR_BL_data()
+{
+  return dIR_BL_range;
+}
 
 void Devastator::FlagPublisher(int x)
 {
@@ -250,11 +190,119 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "devastator_control");
   ROS_INFO("Node Started...");
-
   Devastator devastator;
   ros::Rate r(10);
   while (ros::ok()){
     ros::spinOnce();
+
+    //If no override is detected. Navigate autonomously.
+    if (!devastator.override_flag())
+    {
+      float sonic_FF_zone = 0.30;
+      float sonic_FD_zone = 0.40;
+      float aIR_FR_zone = 0.30;
+      float aIR_FL_zone = 0.30;
+      float dIR_F_zone = 0.0;
+      float dIR_BR_zone = 1.0;
+      float dIR_BL_zone = 1.0;
+
+
+      if (devastator.aIR_FR_data() > aIR_FR_zone && devastator.aIR_FL_data() > aIR_FL_zone)
+      {
+        if (devastator.sonic_FD_data() > sonic_FD_zone && devastator.sonic_FF_data() > sonic_FF_zone and devastator.dIR_F_data()==dIR_F_zone)
+          devastator.FlagPublisher(1);
+        else
+        {
+          if (devastator.dIR_BR_data() && devastator.dIR_BL_data())
+          {
+            devastator.FlagPublisher(5);
+            if (devastator.aIR_FR_data() > devastator.aIR_FL_data())
+              devastator.FlagPublisher(2);
+            else if (devastator.aIR_FL_data() > devastator.aIR_FR_data())
+              devastator.FlagPublisher(3);
+            else
+              devastator.FlagPublisher(4);
+          }
+          else
+          {
+            if (devastator.aIR_FR_data() > devastator.aIR_FL_data())
+              devastator.FlagPublisher(2);
+            else if (devastator.aIR_FL_data() > devastator.aIR_FR_data())
+              devastator.FlagPublisher(3);
+            else
+              devastator.FlagPublisher(4);
+          }
+        }
+
+      }
+
+      else if (devastator.aIR_FR_data() > aIR_FR_zone && devastator.aIR_FL_data() <= aIR_FL_zone)
+        devastator.FlagPublisher(2);
+
+      else if (devastator.aIR_FR_data() <= aIR_FR_zone && devastator.aIR_FL_data() > aIR_FL_zone)
+        devastator.FlagPublisher(3);
+
+      else if (devastator.aIR_FR_data() <= aIR_FR_zone && devastator.aIR_FL_data() <= aIR_FL_zone)
+      {
+        if (devastator.dIR_BR_data() && devastator.dIR_BL_data())
+          {
+            devastator.FlagPublisher(5);
+            if (devastator.aIR_FR_data() > devastator.aIR_FL_data())
+              devastator.FlagPublisher(2);
+            else if (devastator.aIR_FL_data() > devastator.aIR_FR_data())
+              devastator.FlagPublisher(3);
+            else
+              devastator.FlagPublisher(4);
+          }
+
+        else if (!devastator.dIR_BR_data() && !devastator.dIR_BL_data())
+        {
+          if ((devastator.sonic_FD_data() > sonic_FD_zone && devastator.sonic_FF_data() > sonic_FF_zone && devastator.dIR_F_data()==dIR_F_zone) ||
+          (devastator.sonic_FD_data() > sonic_FD_zone && devastator.sonic_FF_data() > sonic_FF_zone && devastator.dIR_F_data() != dIR_F_zone) ||
+          (devastator.sonic_FD_data() > sonic_FD_zone && devastator.sonic_FF_data() <= sonic_FF_zone && devastator.dIR_F_data()==dIR_F_zone) ||
+          (devastator.sonic_FD_data() <= sonic_FD_zone && devastator.sonic_FF_data() > sonic_FF_zone && devastator.dIR_F_data()==dIR_F_zone))
+          {
+            if (devastator.aIR_FR_data() > devastator.aIR_FL_data())
+              devastator.FlagPublisher(2);
+            else if (devastator.aIR_FL_data() > devastator.aIR_FR_data())
+              devastator.FlagPublisher(3);
+            else
+              devastator.FlagPublisher(4);
+          }
+          else
+            devastator.FlagPublisher(6);
+        }
+
+        else if ((!devastator.dIR_BR_data() && devastator.dIR_BL_data()) || (devastator.dIR_BR_data() && !devastator.dIR_BL_data()))
+        {
+          if (devastator.aIR_FR_data() > devastator.aIR_FL_data())
+              devastator.FlagPublisher(2);
+            else if (devastator.aIR_FL_data() > devastator.aIR_FR_data())
+              devastator.FlagPublisher(3);
+            else
+              devastator.FlagPublisher(4);
+        }
+      }
+      else
+        devastator.FlagPublisher(5);
+    }
+    //Else navigate manually.
+    else
+    {
+      float* vel = devastator.vel_data();
+
+      if (vel[0] < 0.0) {
+        devastator.FlagPublisher(5);
+      }else if (vel[0] > 0.0) {
+        devastator.FlagPublisher(1);
+      }else if (vel[1] < 0.0) {
+        devastator.FlagPublisher(2);
+      }else if (vel[1] > 0.0) {
+        devastator.FlagPublisher(3);
+      }else
+        devastator.FlagPublisher(6);
+    }
+
     r.sleep();
   }
   return 0;
